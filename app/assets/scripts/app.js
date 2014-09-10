8566c0b6fcf081a0e4b8845f9f5ad0a1913ecf72
Joel Vardy <info@joelvardy.com>
2014-09-10 10:24:58 +0100

    Added gallery support to posts

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,46 @@
-(function () {
-	console.log('Hello World!');
-})();
+$(function () {
+
+	// Very simple, but better than nothing
+	var screenSize = 'mobile';
+	if ($(window).width() > 800) {
+		screenSize = 'desktop';
+	}
+
+	$('div.gallery').each(function (index, galleryElement) {
+
+		var imageCount = $('img', galleryElement).length,
+			imagesLoaded = 0;
+
+		$(galleryElement).addClass('images-'+imageCount);
+
+		$('img', galleryElement).each(function (index, imageElement) {
+
+			var imageSlug = $(imageElement).data('slug'),
+				imagePath = window.location.pathname+'/'+imageSlug+'/';
+
+			// Rmeove placeholder image element
+			$(imageElement).remove();
+
+			// Load the image
+			var image = new Image();
+			image.addEventListener('load', function () {
+
+				imagesLoaded++;
+
+				console.log('Image loaded', image.width, image.height);
+
+				var photoElement = $('<a class="photo" href="'+imagePath+(screenSize === 'mobile' ? 1200 : 2400)+'"><img src="'+image.src+'" /></a>');
+				$(galleryElement).append(photoElement);
+
+				if (imageCount === imagesLoaded) {
+					$(galleryElement).addClass('loaded');
+				}
+
+			});
+			image.src = imagePath+(screenSize === 'mobile' ? 600 : 1200);
+
+		});
+
+	});
+
+});
