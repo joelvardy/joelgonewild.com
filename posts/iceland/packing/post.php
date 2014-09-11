f278a9a980c59bb7be92b44c72dd9277032bc71f
Joel Vardy <info@joelvardy.com>
2014-09-11 19:58:37 +0100

    Added 'packing for iceland' post

---
+++ posts/iceland/packing/post.php
@@ -0,0 +1,44 @@
+<?php
+
+	$post->title = 'Packing';
+	$post->written = '11th September 2014';
+	$post->heroPhoto = 'hero';
+
+?>
+
+<p>The trip to Iceland has been discussed since October 2013, with myself being involved since February 2014.</p>
+
+<p>I was looking forward to packing well before we left for Iceland, but some last minute freelance work put stop to that. Here I am at 8pm, 6 hours before we leave, having just finished packing.</p>
+
+<div class="gallery">
+
+	<img data-slug="top-down" />
+
+</div>
+
+<p>If you know me, you won't be surprise by the amount of photography, and computer equipment. As usual boxers and socks take up a surprising amount of space. Below is the full list:</p>
+
+<ul>
+	<li>Canon 5D mk2 + 50mm + 70mm - 200mm + 17mm to 40mm</li>
+	<li>dSLR memory cards + remote shutter release + flash</li>
+	<li>Tripod + head</li>
+	<li>GoPro Hero 3+</li>
+	<li>GoPro chesty + pole + tripod mount + batteries + memory cards</li>
+	<li>Several Anker batteries</li>
+	<li>Torch + chargers and leads</li>
+	<li>MacBook Pro + external HDD</li>
+	<li>iPad</li>
+	<li>Toiletries + sunglasses</li>
+	<li>Boxers + socks</li>
+	<li>Jeans + t-shirts + shirt</li>
+	<li>Base layers + walking trousers + jumper + jacket</li>
+	<li>Swim shorts</li>
+	<li>Hat + gloves + snood</li>
+	<li>Walking boots + casual trainers</li>
+</ul>
+
+<div class="gallery">
+
+	<img data-slug="packed" />
+
+</div>
