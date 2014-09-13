e3ca3f8debebb7cd06009153138c59abf67189a8
Joel Vardy <info@joelvardy.com>
2014-09-13 01:32:37 +0100

    Published 'mundane' post

---
+++ posts/iceland/mundane/post.php
@@ -0,0 +1,38 @@
+<?php
+
+	$post->title = 'Mundane';
+	$post->written = '12th September 2014';
+	$post->heroPhoto = 'hero';
+
+?>
+
+<p><strong>Mundane</strong>: lacking interest or excitement; dull: <em>his mundane, humdrum existence</em>.</p>
+
+<p>After about 30 minutes sleep, my alarm clock shrieked to life at the ungodly time of 01:15 this morning. Just time for a cup of tea and some breakfast before being picked up to begin the drive down to Luton airport.</p>
+
+<p>A three hour flight and we have arrived in Iceland, today is just an acclimatisation day, we pick up the hire car, get some food, and go to the cabin to unpack and chill out.</p>
+
+<p>For some reason, a lack of sleep doesn't effect me as much as some people (maybe it's all those late night programming) - but I decide I'm going to explore the local area, and see whether I can get some photos, naturally I drag Jenny along too :)</p>
+
+<div class="gallery">
+
+	<img data-slug="mushroom" />
+	<img data-slug="rocks" />
+
+</div>
+
+<p>On the plane over I was reading a 'shooting stars' eBook, and all day I have been hoping for a clear night so I could put some of the things I had read about into practice. So when it was getting dark, and I saw it was clear I grabbed the camera, tripod, and started shooting some photos, I'm pretty happy of this one:</p>
+
+<div class="gallery">
+
+	<img data-slug="star-trails" />
+
+</div>
+
+<p>But I was even more happy when Tanya saw some green wisps in the sky, and there, on the first night, was the Northern Lights :D</p>
+
+<div class="gallery">
+
+	<img data-slug="green-magic" />
+
+</div>
