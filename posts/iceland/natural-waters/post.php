06a3f3ae41bc9e418d72530ea802c01b0995522a
Joel Vardy <info@joelvardy.com>
2014-09-14 01:06:14 +0100

    Added 'natural waters' post

---
+++ posts/iceland/natural-waters/post.php
@@ -0,0 +1,33 @@
+<?php
+
+	$post->title = 'Natural Waters';
+	$post->written = '13th September 2014';
+	$post->heroPhoto = 'hero';
+
+?>
+
+<p>This morning we headed out to <strong>Strokkur Geyser</strong>, which is a high temperature geothermal area, there is a hole in the earth filled with water, when this water is superheated by the surrounding rock, water flashes to steam, forcing the rest of the water upwards into the air. At the Strokkur geyser this happens every eight to ten minutes.</p>
+
+<div class="gallery">
+
+	<img data-slug="strokkur-geyser" />
+	<img data-slug="valley" />
+
+</div>
+
+<p>At thirty-one meters high the <strong>Gullfoss Waterfall</strong> is a sight to behold, consisting of two waterfalls one eleven meters high, and the second twenty meters. The average water flow is one-hundred and nine cubic meters per second, however at times this can be two-thousand cubic meters per second.</p>
+
+<div class="gallery">
+
+	<img data-slug="gulfoss-waterfall" />
+
+</div>
+
+<p>After arriving back at the cabin we had a lovely dinner cooked by Tanya, followed by watching Despicable Me in the hot tub, and a quick few photos of the sky before bed.</p>
+
+<div class="gallery">
+
+	<img data-slug="hot-tub" />
+	<img data-slug="sky" />
+
+</div>
