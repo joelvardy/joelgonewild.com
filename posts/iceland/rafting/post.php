34f54dd5872666f4834c85515588a1ee38ee1beb
Joel Vardy <info@joelvardy.com>
2014-09-14 21:24:14 +0100

    Added 'rafting' post

---
+++ posts/iceland/rafting/post.php
@@ -0,0 +1,27 @@
+<?php
+
+	$post->title = 'Rafting';
+	$post->written = '14th September 2014';
+	$post->heroPhoto = 'team';
+
+?>
+
+<p>My alarm was set for six in the morning, however in my sleep daze I turned off the alarm and rolled over. This lead to a mass rush around at seven-thirty to get ready and jump in the car to drive to the rafting centre.</p>
+
+<p>Upon arrival we slithered into the wetsuits, donned the life jackets, grabbed a paddle, and got into the old American school bus which took us down to the river a few miles away. During the hour in the water (some of the time inside the raft) we splashed, dived, jumped, and larked about. Really good fun, and surprisingly not too cold!</p>
+
+<p>After rafting we visited Kerio, a 55 meter deep crater formed 6,500 years ago which we walked down into.</p>
+
+<div class="gallery">
+
+	<img data-slug="volcano" />
+
+</div>
+
+<p>On the way back towards Reykjavik the weather (as it had been in the morning) was not very pleasant, although it made for a nice view of the surrounding hills.</p>
+
+<div class="gallery">
+
+	<img data-slug="roadside-fog" />
+
+</div>
