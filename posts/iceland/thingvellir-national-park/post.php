f3c1071b0878c210772de69d73798607e0b42e2f
Joel Vardy <info@joelvardy.com>
2014-09-18 23:54:34 +0000

    Added 'Thingvellir National Park' post

---
+++ posts/iceland/thingvellir-national-park/post.php
@@ -0,0 +1,27 @@
+<?php
+
+	$post->title = 'Thingvellir National Park';
+	$post->written = '18th September 2014';
+	$post->heroPhoto = 'group';
+
+?>
+
+<p>This morning we chilled out a little, went to the shop and bought some food for out last few days, then headed off to Thingvellir National Park in the afternoon.</p>
+
+<p>Thingvellir was the site of the ancient Icelandic parliament, a beautiful area, most striking of all are the faults between the Eurasian and North American tectonic plates which scar the landscape. Mandy of these are filled with surprisingly clear, blue water.</p>
+
+<div class="gallery">
+
+	<img data-slug="water" />
+	<img data-slug="joel" />
+	<img data-slug="reflection" />
+
+</div>
+
+<p>The beautiful area of water shown below is where Women were drowned after being convicted of either incest or prostitution, depending on what account you read.</p>
+
+<div class="gallery">
+
+	<img data-slug="drekkingarhylur" />
+
+</div>
