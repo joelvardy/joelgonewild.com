a8d07868193748f463b7387ca6767c1f0b8f9b93
Joel Vardy <info@joelvardy.com>
2014-09-18 00:01:24 +0000

    Added 'blue lagoon' post

---
+++ posts/iceland/blue-lagoon/post.php
@@ -0,0 +1,24 @@
+<?php
+
+	$post->title = 'Blue Lagoon';
+	$post->written = '17th September 2014';
+	$post->heroPhoto = 'group';
+
+?>
+
+<p>After the <a href="/iceland/busy-day-of-epicness" title="Busy day of Epicness">busy day yesterday</a>, today was a refreshing, and relaxing day at the Blue Lagoon. The lagoon is not natural, and is caused by surplus mineral-rich water from the neighboring geothermal power plant.</p>
+
+<p>We started out in the lagoon, having a walk / swim around, moving between areas of hot and cooler water. After getting a cold beer, and applying a face mask, we took some more silly photos and headed over to the sauna / steam room.</p>
+
+<p>Following our relaxing time in the pool we ate an outstanding three-course meal at the <strong>LAVA Restaurant</strong> within the complex.</p>
+
+<div class="gallery">
+
+	<img data-slug="joel-and-jenny" />
+	<img data-slug="water" />
+	<img data-slug="joel" />
+	<img data-slug="beef" />
+
+</div>
+
+<p>After arriving back at the cabin, we had some birthday Bollinger for Tanyas birthday and planned tomorrows activities&hellip;</p>
