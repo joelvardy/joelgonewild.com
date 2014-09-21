8b92bc4f12c901425824f8447d88649396ce61ea
Joel Vardy <info@joelvardy.com>
2014-09-21 15:57:07 +0000

    Added 'returing home' post

---
+++ posts/iceland/returning-home/post.php
@@ -0,0 +1,71 @@
+<?php
+
+	$post->title = 'Returning Home';
+	$post->written = '21st September 2014';
+	$post->heroPhoto = 'night-sky';
+
+?>
+
+<p>I'm writing this in the back of Steve's car, on the way back to Sheffield, the conclusion of the trip to Iceland has almost been reached. While I have nothing better to do, I'd like to reflect on some of the memorable points of the trip.</p>
+
+<h3>The Surprising</h3>
+
+<p>It's not been cold, yes it's only September, but I was expecting to see frost and negative figures when <a href="/iceland/busy-day-of-epicness" title="Glacier walk">on the glacier</a>, but in reality it has been like the UK, in the teens most days.</p>
+
+<p>It's so relaxed, in the ten days we were in Iceland, we were only stuck in traffic twice; once in heavy fog, and once coming through Monday afternoon <em>rush hour</em> - if you can even call it rush hour.</p>
+
+<p>It's not actually too embarrassing using a GoPro 'selfie pole' when you know you'll never see the people around you again, and you can get some unique shots.</p>
+
+<div class="gallery">
+
+	<img data-slug="strokkur-geyser" />
+
+</div>
+
+<h3>The Frustrating</h3>
+
+<p>I called the first blog post <a href="/iceland/mundane" title="Not a mundane evening">mundane</a>, however I now regret that, as it was the only night we saw the aurora borealis, and that was certainly not mundane.</p>
+
+<p>Having reviewed the photos, learning how to set up the camera, and progressing my post processing skills over the week. I was really looking forward to capturing the aurora borealis. Alas, I'm planning a trip to Finland in early 2016 and I should be able to get some amazing photos then.</p>
+
+<div class="gallery">
+
+	<img data-slug="aurora-borealis" />
+
+</div>
+
+<p>Unfortunately we rented a Ford Focus, and while it was a lovely modern car, having a 4x4 would be opened more opportunities for exploration.</p>
+
+<h3>The Awesome</h3>
+
+<p>The group consensus is certainly that the <a href="/iceland/busy-day-of-epicness" title="Falljokull glacier">glacier walk</a> was the best activity all week. I would really like to do more of a glacier hike in the future.</p>
+
+<div class="gallery">
+
+	<img data-slug="joel-falljokull-glacier" />
+
+</div>
+
+<p>On the seventh day I could finally spell <em>Reykjavik</em> without having to look it up. I suppose that's something :P</p>
+
+<h3>The Tasty</h3>
+
+<p>The cabin we stayed in didn't have an oven, however it did have a barbecue, this and the high price of food in Iceland meant that most of the meals were quite basic, but after being active all day, and driving for hours, simple food tastes so good.</p>
+
+<p>I was all up for trying some whale, but the opportunity didn't arise.</p>
+
+<div class="gallery">
+
+	<img data-slug="barbecue" />
+
+</div>
+
+<h3>Goodbye</h3>
+
+<p>I speak for all four of us when I say we've had a blast. I'm already planning a trip back to Iceland, renting a big 4x4 and camping different areas rather than retuning to Reykjavik each evening.</p>
+
+<div class="gallery">
+
+	<img data-slug="night-cabin" />
+
+</div>
