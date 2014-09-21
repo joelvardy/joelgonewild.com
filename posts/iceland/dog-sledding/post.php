719457786cb2169e30d23361537bc86cdeb35dac
Joel Vardy <info@joelvardy.com>
2014-09-21 00:37:49 +0000

    Added 'dog sledding' post

---
+++ posts/iceland/dog-sledding/post.php
@@ -0,0 +1,27 @@
+<?php
+
+	$post->title = 'Dog Sledding';
+	$post->written = '20th September 2014';
+	$post->heroPhoto = 'hero';
+
+?>
+
+<p>We drove back down part of the South coast this morning, arriving at the only dog sledding centre in Iceland late in the morning, where we were pulled on a summer sled by twelve beautiful dogs.</p>
+
+<p>My favorite by far was Brutus, who at six years old is getting on, but he can, I was told, easily pull 250kg on his own. It was funny when <em>the musher</em> who pilotted the sled, noticed individual dogs slacking off, he would call them by name, and they would start pulling on the lines again.</p>
+
+<div class="gallery">
+
+	<img data-slug="dog-sledding" />
+
+</div>
+
+<p>Just up the road from the dog sledding centre, was a lighthouse, the tallest structure in South Iceland, and of course past the lighthouse was a beach.</p>
+
+<div class="gallery">
+
+	<img data-slug="beach" />
+
+</div>
+
+<p>All day we were teased by clear skys and sunny weather, however despite very little cloud, by eleven pm I was getting doubtful that I would be able to take any further photos of the northern lights. And by midnight I decided to bring the stuff back inside to warm up, ready to pack it back in the camera bag.</p>
