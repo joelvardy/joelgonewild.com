9fcda4e7a7f4641c34221ed60ecce5880c31c7e1
Joel Vardy <info@joelvardy.com>
2015-06-13 17:22:28 +0100

    Added post for Prague day 4

---
+++ posts/prague/relaxing-before-the-long-trip-home/post.php
@@ -0,0 +1,22 @@
+<?php
+
+$post->title = 'Relaxing Before The Long Trip Home';
+$post->written = '11th June 2015';
+$post->heroPhoto = 'metro';
+
+?>
+
+<p>I tried to have a lie in until 10am however around 8am my body was well and truly awake, I decided to get some breakfast and book another massage for later that morning.</p>
+
+<p>After I enjoyed a very relaxing massage we checked out of the hotel and proceeded to the mighty model train exhibition.</p>
+
+<div class="gallery">
+
+    <img data-slug="trains-rock-climbers" />
+    <img data-slug="trains-caravans" />
+
+</div>
+
+<p>The rest of the day was mainly sitting in parks and taking in the nice weather before our time in Prague was through.</p>
+
+<p>16 hours after arriving at Prague airport, we got back to Sheffield, both very tired, and in need of some shut eye.</p>
