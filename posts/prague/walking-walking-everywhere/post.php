e24f5ff52118769fcd3befbee18d35ec086fddc8
Joel Vardy <info@joelvardy.com>
2015-06-13 16:21:43 +0100

    Added post for Prague day 1

---
+++ posts/prague/walking-walking-everywhere/post.php
@@ -0,0 +1,23 @@
+<?php
+
+$post->title = 'Walking Walking Everywhere';
+$post->written = '8th June 2015';
+$post->heroPhoto = 'panorama';
+
+?>
+
+<p>After getting three trains from Sheffield to Standtead, waiting around in the Costa for four hours, and then spending a further four hours in the increasingly busy departure lounge, bemused at travellers rushing to queue for the monorail carriage which was yet to arrive, we were finally given a boarding gate.</p>
+
+<p>It was surprisingly clear as we climbed up over the UK offering really good views down the south coast, before the cloud started to come in over mainland Europe.</p>
+
+<p>Upon arriving in the city centre of Prague we decided to have a look around and see what was happening, within ten minutes we were watching a crew film a fight scene for The Right Europe.</p>
+
+<div class="gallery">
+
+    <img data-slug="cathedral" />
+
+</div>
+
+<p>We walked up to Prague Castle and the cathedral, we ended up walking around most of old town trying to find our hotel after some navigation failures by me. Upon finding the hotel we each got a shower and while I looked for somewhere to eat Ed fell asleep snoring loudly.</p>
+
+<p>We grabbed some food and headed to Petrin Tower and the observatory in the evening. Getting back to the hotel just as the rain started.</p>
