a6420406900e75437fa6f1b0085eafd0a3487f2e
Joel Vardy <info@joelvardy.com>
2015-06-13 16:38:15 +0100

    Added post for Prague day 2

---
+++ posts/prague/shooting/post.php
@@ -0,0 +1,31 @@
+<?php
+
+$post->title = 'Shooting';
+$post->written = '9th June 2015';
+$post->heroPhoto = 'taking-aim';
+
+?>
+
+<p>Getting up "early" (7am) to get some breakfast before the driver picked us up to go to the range shooting.</p>
+
+<p>A few hours later we'd fired nine guns each, and I was the winner, with Ed amazingly managing to miss on each of the ten shots from the Magnum.</p>
+
+<p>Just as we finished up shorting we saw a group of ~20 police training on a range, speaking to the instructor we were told all police in the Czech Republic carry a sidearm, and they must practice several times each year.</p>
+
+<div class="gallery">
+
+    <img data-slug="dragunov-joel" />
+    <img data-slug="marking" />
+    <img data-slug="dragunov-ed" />
+
+</div>
+
+<p>We went to find the Vyšehrad casemates, and three hours and after having visited the police museum we did manage to find them. The casemates are tunnels which were used to secretly transport troops, and store munitions within the armaments.</p>
+
+<p>Having reserved a table at Pagana, we arrived promptly and within two minutes had both eaten a snail for the first time. I'd describe it as a chewy mushroom.</p>
+
+<div class="gallery">
+
+    <img data-slug="dinner" />
+
+</div>
