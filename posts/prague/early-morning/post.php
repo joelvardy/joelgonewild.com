1c25d989c94ae59019dbd871a24219a4dfaa871a
Joel Vardy <info@joelvardy.com>
2015-06-13 17:08:29 +0100

    Added post for Prague day 3

---
+++ posts/prague/early-morning/post.php
@@ -0,0 +1,38 @@
+<?php
+
+$post->title = 'Early Morning';
+$post->written = '10th June 2015';
+$post->heroPhoto = 'high-vantage-selfie';
+
+?>
+
+<p>My alarm woke me from my slumber at 4am the reason for this intrusion was allowing me to take some photos before the throngs of tourists arrived around Charles Bridge.</p>
+
+<p>After a successful photography session I headed back to the hotel and got into bed at 7am - another two hours of rest before starting the days activities.</p>
+
+<div class="gallery">
+
+    <img data-slug="charles-bridge-wide" />
+    <img data-slug="charles-bridge" />
+
+</div>
+
+<p>We first went to the Army Museum which covered the 19th to 20th century.<br />However, en route to the river our tram was part of a minor crash with a vehicle, my first tram crash.</p>
+
+<div class="gallery">
+
+    <img data-slug="peddling-hard" />
+
+</div>
+
+<p>After having the thickest soup-like hot chocolate I have ever seen, we took a walk up the tower on the south side of the Charles Bridge.</p>
+
+<p>Returning to the hotel a little before 5pm we chilled out before having a very relaxing thai massage at the hotel spa.</p>
+
+<p>In this evening we grabbed a burger at The Tavern and visited the TV tower to whiteness some spectacular panoramic views of the city.</p>
+
+<div class="gallery">
+
+    <img data-slug="group-selfie" />
+
+</div>
