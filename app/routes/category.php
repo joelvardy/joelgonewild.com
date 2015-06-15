ff19dc60d90e7a55806b9bdaace65a338ee3ddef
Joel Vardy <info@joelvardy.com>
2015-06-15 09:33:44 +0100

    Added some comments explaining checks

--- app/routes/category.php
+++ app/routes/category.php
@@ -4,6 +4,7 @@ use Travel\Blog;
 
 Flight::route('GET /category/@category', function ($category_slug) {
 
+    // Ensure category exists
 	$category = Blog::category($category_slug);
 	if ( ! $category) Flight::halt(404);
 
--- app/routes/post.php
+++ app/routes/post.php
@@ -4,9 +4,11 @@ use Travel\Blog;
 
 Flight::route('GET /@category/@post', function ($category_slug, $post_slug) {
 
+    // Ensure category exists
 	$category = Blog::category($category_slug);
 	if ( ! $category) Flight::halt(404);
 
+    // Ensure post exists
 	$post = Blog::post($category_slug, $post_slug);
 	if ( ! $post) Flight::halt(404);
 
