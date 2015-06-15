72c364b22ff9e3e3f498d6324e685b5cd1c045a4
Joel Vardy <info@joelvardy.com>
2015-06-15 09:32:26 +0100

    Added some comments regarding resizing photos

--- app/routes/photo.php
+++ app/routes/photo.php
@@ -5,14 +5,18 @@ use Travel\Photo;
 
 Flight::route('GET /@category/@post/@photo(/@size)(.jpg)', function ($category_slug, $post_slug, $photo_slug, $size) {
 
+    // Ensure category exists
 	$category = Blog::category($category_slug);
 	if ( ! $category) Flight::halt(404);
 
+    // Ensure post exists
 	$post = Blog::post($category_slug, $post_slug);
 	if ( ! $post) Flight::halt(404);
 
+    // Ensure photo exists
 	if ( ! isset($post->photos[$photo_slug])) Flight::halt(404);
 
+    // Get requested photo size
 	$size = (integer) ($size === null ? 800 : $size);
 
 	try {
