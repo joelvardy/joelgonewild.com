c5bb84a47acb94f82d32cc3a0074a42401e41196
Joel Vardy <info@joelvardy.com>
2016-01-13 13:28:45 +0000

    Agged Riga post for 10th Jan 2016

---
+++ posts/riga/birthday/post.php
@@ -0,0 +1,28 @@
+<?php
+
+$post->title = 'My Birthday';
+$post->written = '10th January 2016';
+$post->heroPhoto = 'view-looking-south-east-from-st-peters-church';
+
+?>
+
+<p>I'd set my alarm just before sunrise, upon waking up I looked out of the window, which had largely frosted over on the outside, and saw that the cloud cover was so thick there was no point heading out for some golden sun.</p>
+
+<p>It was my 23rd Birthday, and I'd brought a few cards from home, so I opened them and thanked people back home.</p>
+
+<p>I left the hotel at 11:30 after relaxing in the morning. While not as cold as the previous day it was necessary to keep moving to keep warm, this means I've covered most of the city already.</p>
+
+<div class="gallery">
+
+    <img data-slug="daugava-rail-bridge">
+    <img data-slug="view-from-st-pauls-cath">
+
+</div>
+
+<p>The photos above are from when I went up to the St. Peter's Church observation platform which has spectacular views over old town, although exposed 72m above the ground means taking photos in-between warming fingers back up.</p>
+
+<div class="gallery">
+
+    <img data-slug="parnorama-from-st-peters-church">
+
+</div>
