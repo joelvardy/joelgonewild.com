8789a1714e007248c7ca431a786b23772b9a5356
Joel Vardy <info@joelvardy.com>
2016-01-13 13:49:41 +0000

    Agged Riga post for 11th Jan 2016

---
+++ posts/riga/getting-high/post.php
@@ -0,0 +1,30 @@
+<?php
+
+$post->title = 'Getting High';
+$post->written = '11th January 2016';
+$post->heroPhoto = 'panorama-from-skylight-bar';
+
+?>
+
+<p>No I wasn't doing drugs.. It's great to look down on a city from an observation deck, and Riga has several places you can do that from.</p>
+
+<p>Leaving the hotel I headed to the Latvian Academy of Sciences observation deck, I was the only person up there leaving me to enjoy the stunning view, however despite it not being as cold today (-6°c) there is a lot more wind (wind chill of about -17°c) so I didn't spend more than half an hour on the exposed observation deck.</p>
+
+<p>I had planned to visit the photography museum, however it was closed (I should have checked yesterday.) So after walking around old town I headed to the Skyline Bar to watch the snow fall from the 26th floor.</p>
+
+<div class="gallery">
+
+    <img data-slug="view-from-skyline-bar">
+
+</div>
+
+<p>After watching the snow fall for hours, and drinking quite a lot of alcohol, I went for a walk in the crunchy snow :)</p>
+
+<div class="gallery">
+
+    <img data-slug="skyline-bar">
+    <img data-slug="radisson-blu-hotel">
+    <img data-slug="pilsetas-kanals">
+    <img data-slug="klostera-iela">
+
+</div>
