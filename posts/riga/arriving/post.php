b16dba0be80863c75c554dc256886f2379d8d4c5
Joel Vardy <info@joelvardy.com>
2016-01-13 13:12:46 +0000

    Agged Riga post for 9th Jan 2016

---
+++ posts/riga/arriving/post.php
@@ -0,0 +1,26 @@
+<?php
+
+$post->title = 'Arriving';
+$post->written = '9th January 2016';
+$post->heroPhoto = 'house-of-the-blackheads';
+
+?>
+
+<p>I touched down at Riga International Airport ahead of schedule despite leaving the UK 15 minutes late, disembarking the plane there was light snowfall. I walked over to the bus stop, getting a 10 journey ticket (€10.9) and got onto the #22 bus getting off just outside my hotel.</p>
+
+<p>I enjoyed walking on crunchy snow (which wasn't imminently about to melt - as it usually is in British cities) - I was glad I'd made the choice to buy a new pair of walking boots!</p>
+
+<div class="gallery">
+
+    <img data-slug="snowy-bastejkalna-park">
+    <img data-slug="icy-streetlamp">
+
+</div>
+
+<p>I walked around the Old Town, taking photos was broken up by having to stow the camera and warm my fingers back up (despite wearing a pair of gloves.)</p>
+
+<div class="gallery">
+
+    <img data-slug="frozen-daugava-river">
+
+</div>
