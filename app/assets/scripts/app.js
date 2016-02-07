7bd12acef50a827ecfd98a6ad5b36b1889b6941b
Joel Vardy <info@joelvardy.com>
2016-02-07 22:43:10 +0000

    Add functionality for inline linked photos

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,32 +1,32 @@
 $(function () {
 
 
-	// Very simple, but better than nothing
-	var screenSize = 'mobile';
-	if ($(window).width() > 800) {
-		screenSize = 'desktop';
-	}
+    // Very simple, but better than nothing
+    var screenSize = 'mobile';
+    if ($(window).width() > 800) {
+        screenSize = 'desktop';
+    }
 
 
     // Close lightbox on escape key
-	$(document).keyup(function (event) {
-		if (event.keyCode == 27) {
-			$('body').lightbox('close');
-		}
-	});
+    $(document).keyup(function (event) {
+        if (event.keyCode == 27) {
+            $('body').lightbox('close');
+        }
+    });
 
 
     // Load hero image (responsive)
-	$('.photo.hero').each(function (index, heroPhotoElement) {
+    $('.photo.hero').each(function (index, heroPhotoElement) {
 
-		var imagePath = $(heroPhotoElement).data('path');
+        var imagePath = $(heroPhotoElement).data('path');
 
-		// Load the image
-		var image = new Image();
-		image.addEventListener('load', function () {
+        // Load the image
+        var image = new Image();
+        image.addEventListener('load', function () {
 
-			$(heroPhotoElement).css('background-image', 'url('+image.src+')');
-			$(heroPhotoElement).addClass('loaded');
+            $(heroPhotoElement).css('background-image', 'url(' + image.src + ')');
+            $(heroPhotoElement).addClass('loaded');
 
             // If the hero image is at the top of a post allow it to be opened in a lightbox
             if ($(heroPhotoElement).hasClass('hero-post')) {
@@ -36,56 +36,65 @@ $(function () {
                 });
             }
 
-		});
-		image.src = imagePath+'/'+(screenSize === 'mobile' ? 600 : 1200)+'.jpg';
+        });
+        image.src = imagePath + '/' + (screenSize === 'mobile' ? 600 : 1200) + '.jpg';
 
-	});
+    });
+
+
+    // Load inline image (responsive)
+    $('img.inline').each(function (index, imgElement) {
+
+        var imagePath = $(imgElement).data('path');
+        imgElement.src = window.location.pathname + '/' + imagePath + '/' + (screenSize === 'mobile' ? 600 : 1200) + '.jpg';
+
+    });
 
 
     // Load gallery images (responsive)
-	$('div.gallery').each(function (index, galleryElement) {
+    $('div.gallery').each(function (index, galleryElement) {
 
-		var imageCount = $('img', galleryElement).length,
-			imagesLoaded = 0;
+        var imageCount = $('img', galleryElement).length,
+            imagesLoaded = 0;
 
-		$(galleryElement).addClass('images-'+imageCount);
+        $(galleryElement).addClass('images-' + imageCount);
 
         // Iterate through each image
-		$('img', galleryElement).each(function (index, imageElement) {
+        $('img', galleryElement).each(function (index, imageElement) {
 
-			var imageSlug = $(imageElement).data('slug'),
-				imagePath = window.location.pathname+'/'+imageSlug;
+            var imageSlug = $(imageElement).data('slug'),
+                imagePath = window.location.pathname + '/' + imageSlug;
 
-			// Remove placeholder image element
-			$(imageElement).remove();
+            // Remove placeholder image element
+            $(imageElement).remove();
 
-			// Load the image
-            // NOTE: This will not preserve the order of images in the gallery
-			var image = new Image();
-			image.addEventListener('load', function () {
+            // Load the image
+            // TODO: Preserve the order of images in the gallery
+            var image = new Image();
+            image.addEventListener('load', function () {
 
-				imagesLoaded++;
+                imagesLoaded++;
 
-				var photoElement = $('<a class="photo" href="'+imagePath+'/'+(screenSize === 'mobile' ? 1200 : 2400)+'.jpg"><img src="'+image.src+'" /></a>');
-				$(galleryElement).append(photoElement);
+                var photoElement = $('<a class="photo" href="' + imagePath + '/' + (screenSize === 'mobile' ? 1200 : 2400) + '.jpg"><img src="' + image.src + '" /></a>');
+                $(galleryElement).append(photoElement);
 
-				photoElement.click(function () {
-					$('body').lightbox('open', $(this).attr('href'));
-					return false;
-				});
+                photoElement.click(function () {
+                    $('body').lightbox('open', $(this).attr('href'));
+                    return false;
+                });
 
                 // TODO: Allow each gallery to be navigated from lightbox
 
-				if (imageCount === imagesLoaded) {
-					$(galleryElement).addClass('loaded');
-				}
+                if (imageCount === imagesLoaded) {
+                    $(galleryElement).addClass('loaded');
+                }
 
-			});
-			image.src = imagePath+'/'+(screenSize === 'mobile' ? 600 : 1200)+'.jpg';
+            });
+            image.src = imagePath + '/' + (screenSize === 'mobile' ? 600 : 1200) + '.jpg';
 
-		});
+        });
 
-	});
+    });
 
 
 });
