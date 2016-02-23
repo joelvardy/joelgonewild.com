1b7bd3a0404a1871adfe74a61ad05a4d6fe8906e
Joel Vardy <info@joelvardy.com>
2016-02-23 08:32:01 +0000

    [WIP] Added Berlin post

---
+++ posts/germany-belgium/berlin/post.php
@@ -0,0 +1,11 @@
+<?php
+
+    $post->title = 'Berlin';
+    $post->written = '18th August 2014';
+    $post->heroPhoto = 'group';
+
+?>
+
+<p>Just two days after landing at Luton after <a href="/category/morzine" title="Mountain biking in Morzine">my trip to The Alps</a>, I was back flying out from Luton en route to Berlin.</p>
+
+<p>I almost always take my dSLR / GoPros away with me when I travel - however this wasn't one of those trips, I didn't want to spend loads of time composing photos, and didn't want to worry about leaving my dSLR in a tent. So I was relying on my Nexus 5 for photos.</p>
